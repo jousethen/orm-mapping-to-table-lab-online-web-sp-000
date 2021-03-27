1e644121b958ceb43b69f78bad420485f50b1dac
Jouse then <jthen93@gmail.com>
2021-03-27 16:53:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,16 @@
 class Student
-  attr_accessor :id, :name, :grade
+  attr_accessor :name, :grade 
+  attr_reader :id
+  
   
   def initialize(name, grade, id = nil)
+    @name = name
+    @grade = grade
+  end
+    
+  def self.create_table()
+    sql = <-- SQL 
+            CREATE
+  end
   
 end
