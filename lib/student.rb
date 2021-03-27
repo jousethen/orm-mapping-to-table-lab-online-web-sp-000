971b40f5cb9ba8de2c9ed6f53212e6a764997b8d
Jouse then <jthen93@gmail.com>
2021-03-27 17:12:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,6 +33,8 @@ class Student
             VALUES (?,?)
           SQL
           
-    DB[:conn].execute(sql, @name, @grade)
+    DB[:conn].execute(sql, self.name, self.grade)
+    
+    @id = 
   end
 end
